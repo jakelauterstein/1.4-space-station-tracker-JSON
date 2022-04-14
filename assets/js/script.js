c5cc94a1313cc47aaec0b0ddd7265f7629ebd856
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-04-13 21:28:34 -0500

    added latitude and longitude tracking and setInterval functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,58 @@
-async getData
+const api_url = "https://api.wheretheiss.at/v1/satellites/25544"
+
+async function getISS() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const {latitude, longitude} = data
+
+    document.getElementById('lat').textContent = latitude
+    document.getElementById('lon').textContent = longitude
+
+
+    console.log(data);
+    console.log(latitude);
+    console.log(longitude);
+    var i = 0;
+    i++;
+
+    if (i > 4) {
+        clearInterval(myInterval);
+    }
+}
+
+// getISS();
+
+myInterval = setInterval(getISS, 1000);
+
+
+
+
+// get advice on how to utilize clear interval with this
+
+// const api_url = "https://api.wheretheiss.at/v1/satellites/25544"
+
+// async function getISS() {
+//     const response = await fetch(api_url);
+//     const data = await response.json();
+//     const {latitude, longitude} = data
+
+//     document.getElementById('lat').textContent = latitude
+//     document.getElementById('lon').textContent = longitude
+
+
+//     console.log(data);
+//     console.log(latitude);
+//     console.log(longitude);
+//     var i = 0;
+//     i++;
+
+//     if (i > 4) {
+//         clearInterval(myInterval);
+//     }
+// }
+
+// // getISS();
+
+// myInterval = setInterval(getISS, 1000);
+
+
