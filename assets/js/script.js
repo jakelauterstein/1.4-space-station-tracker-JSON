0702127a62c304d4c583d3e1ecc55509192f8946
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-04-14 20:57:07 -0500

    refactor, integrate setInterval, final touch ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
+// making a map and tiles
+
 const mymap = L.map('issMap').setView([0, 0], 1);
+const attribution = 
+    '&copy; <a href= "https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tiles = L.tileLayer(tileUrl, {attribution}  )
+    tiles.addTo(mymap);
+
+// making a marker with a custom icon
 
 const issIcon = L.icon({
     iconUrl: '320px-International_Space_Station.svg.png',
@@ -6,29 +16,27 @@ const issIcon = L.icon({
     iconAnchor: [25, 16],
 });
 
-// L.marker([50.505, 30.57], {icon: issIcon}).addTo(mymap);
-
 const marker = L.marker([0, 0], { icon: issIcon}).addTo(mymap)
 
-const attribution = 
-    '&copy; <a href= "https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 
-    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    const tiles = L.tileLayer(tileUrl, {attribution}  )
-    tiles.addTo(mymap);
-    const api_url = "https://api.wheretheiss.at/v1/satellites/25544"
+const api_url = "https://api.wheretheiss.at/v1/satellites/25544"
+
+let firstTime = true
 
 async function getISS() {
     const response = await fetch(api_url);
     const data = await response.json();
     const {latitude, longitude} = data
 
-    document.getElementById('lat').textContent = latitude
-    document.getElementById('lon').textContent = longitude
+    document.getElementById('lat').textContent = latitude.toFixed(2)
+    document.getElementById('lon').textContent = longitude.toFixed(2)
 
-    // L.marker([latitude, longitude]).addTo(mymap)
     marker.setLatLng([latitude, longitude]);
-
+    if (firstTime) {
+        mymap.setView([latitude, longitude], 1.1)
+        firstTime = false;
+    }
+    
 
     console.log(data);
     console.log(latitude);
@@ -36,12 +44,10 @@ async function getISS() {
     var i = 0;
     i++;
 
-    if (i > 4) {
-        clearInterval(myInterval);
-    }
+
 }
 
-// getISS();
+getISS();
 
 myInterval = setInterval(getISS, 1000);
 
